8d97da6479655636790187778e0a9451973c2cb3
Tu Inmobiliaria Web <tuinmobiliariaweb@hotmail.com>
2024-02-07 06:51:14 -0300

    mas

--- App.js
+++ App.js
@@ -1,20 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text> ¡PUDE CARGAR LA APP -- "gracias"!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  import { StatusBar } from 'expo-status-bar';
+  import { useState } from 'react';
+  import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+  
+  
+    const App = () => {
+     
+      const [newTask,setNewTask] = useState({})
+      const [tasks,setTasks] = useState ([])
+  
+  
+    const addTask = () =>{
+      console.log("tarea")
+    }
+  
+    const onHandlerTitle = (t)=>{
+       setNewTask({title:t})
+       console.log(newTask)
+    }
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+        <TextInput onChangeText={onHandlerTitle} placeholder='Ingresar tarea' style={styles.input}/>
+        <Button  color="#3921F5"title='ADD' onPress={addTask}/>
+      </View>
+          <View style={styles.tasksContainer}>
+            <View style={styles.taskcard}>
+              <Text style={styles.text}>Tarea 1</Text>
+              <Button title='DEL'/>
+            </View>
+            <View style={styles.taskcard}>
+              <Text style={styles.text}>Tarea 2</Text>
+              <Button title='DEL'/>
+            </View>
+            <View style={styles.taskcard}>
+              <Text style={styles.text}>Tarea 3</Text>
+              <Button title='DEL'/>
+            </View>
+            <View style={styles.taskcard}>
+              <Text style={styles.text}>Tarea 4</Text>
+              <Button title='DEL'/>
+            </View>
+           </View>
+      </View>
+    );
+  }
+  
+  const styles= StyleSheet.create({
+    container: {
+      backgroundColor:"#C5C5C5",
+      flex:1, 
+      paddingTop:30
+    },
+    inputContainer:{
+      flexDirection:"row",
+      alignItems:"center",
+      justifyContent:"space-evenly",
+    },
+  
+    input:{
+    width:250,
+    borderBottomWidth:2,
+    margin:10,
+    paddingVertical:5,
+    paddingHorizontal:10
+          },
+      
+    tasksContainer:{
+      alignItems:"center",
+      gap:25,
+      padding:10
+    },
+  
+    taskcard:{
+      flexDirection:"row",
+      backgroundColor:"#F00000",
+      padding:20,
+      borderRadius:5,
+      gap:200,
+      alignItems:"center"
+      
+    },
+  
+    text:{
+      fontSize:20,
+      width:"70%",
+      color:"white"
+    },
+  
+        })
+      
+  
